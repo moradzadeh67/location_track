b31388079826b682e858e0931b8d871bb2e84a03
reza <moradzade.mohamadreza@gmail.com>
2025-08-22 13:22:23 -0700

    sixth commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:location_track/providers/point_provider.dart';
 import 'package:location_track/router/app_router.dart';
 import 'package:provider/provider.dart';
-
-
-
 
 void main() {
   runApp(const MyApp());
@@ -13,22 +11,32 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (_) => PointProvider(),
       child: MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Point App',
         theme: ThemeData(
-
+          fontFamily: 'IranSans',
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         ),
+
+
+        supportedLocales: const [
+          Locale('en'),
+          Locale('fa'),
+        ],
+        localizationsDelegates: const [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        locale: const Locale('fa'),
+
         initialRoute: AppRouter.pointListRoute,
         onGenerateRoute: AppRouter.onGenerateRoute,
       ),
     );
   }
 }
-
-
